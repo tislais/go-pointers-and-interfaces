b5bb0f0f28382acf16b15f715d8de8ed25c963f4
Tis Lais <tis.lais@radarfirst.com>
2022-04-11 12:44:36 -0700

    add name change behavior with setter functions

--- main.go
+++ main.go
@@ -14,6 +14,18 @@ func main() {
 	bmw := Car{name: "bmw is the best car"}
 	volvo := Truck{name: "volvo is the best truck"}
 
+	// passing the whole struct and working with a copy
+	bmw.name = changeNameOfCar(bmw)
+	volvo.name = changeNameOfTruck(volvo)
+
 	fmt.Println(bmw.name)
 	fmt.Println(volvo.name)
+}
+
+func changeNameOfCar(c Car) string {
+	return c.name + "\n\t... changed with simple function"
+}
+
+func changeNameOfTruck(t Truck) string {
+	return t.name + "\n\t... changed with simple function"
 }

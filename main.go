a6642f98cff4b1d8150c0640fd180ef68d8aa492
Tis Lais <tis.lais@radarfirst.com>
2022-04-12 16:55:29 -0700

    add methods

--- main.go
+++ main.go
@@ -18,14 +18,51 @@ func main() {
 	bmw.name = changeNameOfCar(bmw)
 	volvo.name = changeNameOfTruck(volvo)
 
+	// passing reference to original struct and working with that
+	changeNameOfCarUsingPointer(&bmw)
+	changeNameOfTruckUsingPointer(&volvo)
+
+	bmw.name = bmw.changeNameUsingMethod()
+	volvo.name = volvo.changeNameUsingMethod()
+
+	bmw.changeNameUsingMethodWithPointer()
+	volvo.changeNameUsingMethodWithPointer()
+
 	fmt.Println(bmw.name)
 	fmt.Println(volvo.name)
 }
 
+// working with a copy
 func changeNameOfCar(c Car) string {
 	return c.name + "\n\t... changed with simple function"
 }
 
 func changeNameOfTruck(t Truck) string {
 	return t.name + "\n\t... changed with simple function"
+}
+
+// working with pointers
+func changeNameOfCarUsingPointer(c *Car) {
+	c.name = c.name + "\n\t... changed using pointer"
+}
+
+func changeNameOfTruckUsingPointer(t *Truck) {
+	t.name = t.name + "\n\t... changed using pointer"
+}
+
+// using a method
+func (c Car) changeNameUsingMethod() string {
+	return c.name + "\n\t... changed with method"
+}
+
+func (t Truck) changeNameUsingMethod() string {
+	return t.name + "\n\t... changed with method"
+}
+
+func (c *Car) changeNameUsingMethodWithPointer() {
+	c.name = c.name + "\n\t... changed with method using pointer"
+}
+
+func (t *Truck) changeNameUsingMethodWithPointer() {
+	t.name = t.name + "\n\t... changed with method using pointer"
 }
